0decb1e87fc347e9c7c5d915b4bc391560b08a6e
louisbaggins <44960191+louisbaggins@users.noreply.github.com>
2018-12-07 22:01:37 -0200

    Update somador.vhd

--- somador.vhd
+++ somador.vhd
@@ -1,15 +1,16 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
 entity somador is
 port (	x, w	: in std_logic_vector(7 downto 0);
-		 	y	: out std_logic_vector(7 downto 0));
+		 	z	: out std_logic_vector(7 downto 0));
 end somador;
 
 architecture comportamental of somador is
 begin
 		
-		y <= x+w;
+		z <= std_logic_vector(unsigned(x) + unsigned(w));
+
 		 		  
 end comportamental;
